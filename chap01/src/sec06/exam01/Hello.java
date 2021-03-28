1a80c2b7c2799c60998b84a942857293a07ff578
kshel <kshel@WON-PC>
2021-03-28 13:45:43 +0900

    chap01

--- chap01/src/sec06/exam01/Hello.java
+++ chap01/src/sec06/exam01/Hello.java
@@ -2,6 +2,6 @@ package sec06.exam01;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello, welcome to the java world!");
+		System.out.println("Hello, Java");
 	}
 }
